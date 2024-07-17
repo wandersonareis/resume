aa066eeba1448a16757d4e1b77e99949096f16b7
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-17 19:48:49 -0300

    add footer and header components on components module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterBarComponent } from './navegations/footer-bar/footer-bar.component';
+import { HeaderBarComponent } from './navegations/header-bar/header-bar.component';
 
 
 
@@ -8,6 +9,11 @@ import { FooterBarComponent } from './navegations/footer-bar/footer-bar.componen
   declarations: [],
   imports: [
     CommonModule,
+    HeaderBarComponent,
+    FooterBarComponent
+  ],
+  exports: [
+    HeaderBarComponent,
     FooterBarComponent
   ]
 })
