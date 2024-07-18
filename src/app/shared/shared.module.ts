2c5515fb30b9707ec5f88d2ea4909625a88eeb40
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-18 00:28:24 -0300

    add new components to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IconModule } from '../../../projects/icon/src/public-api';
 import { LinkButtonComponent } from './link-button/link-button.component';
 import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import { MailLinkComponent } from './mail-link/mail-link.component';
+import { CopyButtonComponent } from './copy-button/copy-button.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { BadgeComponent } from './badge/badge.component';
+import { TitleAndDescriptionComponent } from './title-and-description/title-and-description.component';
 
 
 
@@ -12,12 +17,22 @@ import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
     CommonModule,
     IconModule,
     LinkButtonComponent,
-    ThemeToggleComponent
+    ThemeToggleComponent,
+    MailLinkComponent,
+    CopyButtonComponent,
+    SocialLinksComponent,
+    BadgeComponent,
+    TitleAndDescriptionComponent
   ],
   exports: [
     IconModule,
     LinkButtonComponent,
-    ThemeToggleComponent
+    ThemeToggleComponent,
+    MailLinkComponent,
+    CopyButtonComponent,
+    SocialLinksComponent,
+    BadgeComponent,
+    TitleAndDescriptionComponent
   ]
 })
 export class SharedModule { }
