6a42a454cc0cb0a45bc58979528c91c8158bfbc4
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-19 23:24:06 -0300

    add routes for projects on app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HeroProjectsComponent } from './components/hero-projects/hero-projects.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         pathMatch: 'full'
+    },
+    {
+        path: 'projects/:tech',
+        component: HeroProjectsComponent
+    },
+    {
+        path: 'projects',
+        component: HeroProjectsComponent
     },
     {
         path: '**',
