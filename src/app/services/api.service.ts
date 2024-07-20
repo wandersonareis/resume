f493fd6c5993586a325f64b16e31004a56bae715
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-20 16:08:21 -0300

    refact apiService with cache request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,67 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { HeroProjectsData } from '../components/hero-projects/hero-projects.component';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private url = "https://api.npoint.io/90a4e87f89c17143fd2a";
-
+  private cacheMap: Map<string, any> = new Map();
   constructor(private http: HttpClient) { }
 
-  getHeaderData<T>(): Observable<T> {
-    const navbarDataUrl: string = this.url + "/navbar";
-    return this.http.get<T>(navbarDataUrl);
+  getFromHttp<T>(url: string): Observable<T> {
+    return this.http.get<T>(url);
   }
-  getHeroCardData<T>(): Observable<T> {
-    const heroDataUrl: string = this.url + "/profile";
-    return this.http.get<T>(heroDataUrl);
-  }
-  getHeroAboutData<T>(): Observable<T> {
-    const heroAboutDataUrl: string = this.url + "/about";
-    return this.http.get<T>(heroAboutDataUrl);
-  }
-  getHeroSkillsData<T>(): Observable<T> {
-    const heroSkillsUrl: string = this.url + "/skills";
-    return this.http.get<T>(heroSkillsUrl);
-  }
-  getHeroJobsData<T>(): Observable<T> {
-    const jobsDataUrl: string = this.url + "/experiences";
-    return this.http.get<T>(jobsDataUrl);
-  }
-  getHeroProjectsData<T>(): Observable<T> {
-    const projectsDataUrl: string = this.url + "/projects";
-    return this.http.get<T>(projectsDataUrl);
-  }
-  getHeroProjectsByFilterData(tech: string | null): Observable<HeroProjectsData> {
-    return this.getHeroProjectsData<HeroProjectsData>()
-      .pipe(
-        map(data => {
-          const projects = data.projectsList;
 
-          if (!tech) return data;
+  getCachedData<T>(key: string, url: string): Observable<T> {
+    let dataObservable = Observable.create((observer: Observer<T>) => {
+      if (this.cacheMap.has(key)) {
+        console.log(`## returning existing ${key} data`);
 
-          const filteredProjects = projects.filter(project =>
-            project.technologies.includes(tech)
-          );
+        observer.next(this.cacheMap.get(key));
+        return observer.complete();
+      }
+      console.log(`** ${key} data do not yet exist; fetching from rest api...`);
 
-          const newData = Object.assign({}, data, { projectsList: filteredProjects })
-          return filteredProjects.length > 0 ? newData : data
-        })
-      );
-  }
-  getTestimonialsData<T>(): Observable<T> {
-    const testimonialsDataUrl: string = this.url + "/testimonials";
-    return this.http.get<T>(testimonialsDataUrl);
-  }
-  getContactData<T>(): Observable<T> {
-    const contactDataUrl: string = this.url + "/contact";
-    return this.http.get<T>(contactDataUrl);
-  }
-  getFooterData<T>(): Observable<T> {
-    const footerDataUrl: string = this.url + "/footer";
-    return this.http.get<T>(footerDataUrl);
+      this.getFromHttp<T>(url).subscribe(data => {
+        this.cacheMap.set(key, data);
+        observer.next(data);
+        return observer.complete();
+      });
+
+    })
+
+    return dataObservable
   }
 }
