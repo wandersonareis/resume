ee990228a071e88fdf93f2bbb8cd87197fa362cf
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-22 00:56:04 -0300

    add language selector on header logo

--- src/app/components/navegations/header-bar/header-logo/header-logo.component.ts
+++ src/app/components/navegations/header-bar/header-logo/header-logo.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../../../../shared/shared.module';
 
 @Component({
   selector: 'header-logo',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    SharedModule
   ],
   templateUrl: './header-logo.component.html',
   styleUrl: './header-logo.component.css'
