c0645764da27d3b565a3e98e726e39033e427e36
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-23 20:18:04 -0300

    use github actions to create env file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ComponentsModule } from './components/components.module';
 import { SharedModule } from './shared/shared.module';
+import dotenv from 'dotenv';
 
+dotenv.config();
 @Component({
   selector: 'app-root',
   standalone: true,
