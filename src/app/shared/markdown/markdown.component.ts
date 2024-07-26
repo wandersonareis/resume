dd33a4cbe5954106f1f0b743f4fae989d72870af
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-25 22:22:45 -0300

    use html sanitizer for security

--- src/app/shared/markdown/markdown.component.ts
+++ src/app/shared/markdown/markdown.component.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
 import { MarkdownPipe } from '../pipes/markdown.pipe';
 import { CommonModule } from '@angular/common';
+import { SafeHtmlPipe } from '../../../../projects/icon/src/lib/safe-html.pipe';
 
 @Component({
   selector: 'markdown',
   standalone: true,
   imports: [
     CommonModule,
-    MarkdownPipe
+    MarkdownPipe,
+    SafeHtmlPipe
   ],
   templateUrl: './markdown.component.html',
   styleUrl: './markdown.component.css'
