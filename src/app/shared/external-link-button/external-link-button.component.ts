e05aed75881c5b1de215cc9db473322765ec5062
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-28 14:58:37 -0300

    change decorator input to input signal
    use in external link button component
    title and description component

--- src/app/shared/external-link-button/external-link-button.component.ts
+++ src/app/shared/external-link-button/external-link-button.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { IconComponent } from "../../../../projects/icon/src/lib/icon/icon.component";
 
 @Component({
   selector: 'external-link-button',
   standalone: true,
   imports: [IconComponent],
-  templateUrl: './external-link-button.component.html',
+  template: `
+  <a type="button" [href]="link()">
+  <icon
+    name="ci:external-link"
+    size="24"
+    class="text-theme-600 hover:text-theme-800 dark:hover:text-theme-600-dark"
+  ></icon>
+</a>
+`,
   styleUrl: './external-link-button.component.css'
 })
 export class ExternalLinkButtonComponent {
-  @Input() link!: string
+  link = input<string | undefined>();
 }
