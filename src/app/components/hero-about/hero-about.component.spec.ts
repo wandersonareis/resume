f12aa40657b548d3a06c1205f70509f6527a7633
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-07-28 21:26:52 -0300

    tests hero about component

--- src/app/components/hero-about/hero-about.component.spec.ts
+++ src/app/components/hero-about/hero-about.component.spec.ts
@@ -1,9 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroAboutComponent } from './hero-about.component';
+import { ComponentRef, DebugElement } from '@angular/core';
+import { mocked_about_me } from '../../shared/_spec-tools/mock-api.service';
+import { By } from '@angular/platform-browser';
 
 describe('HeroAboutComponent', () => {
   let component: HeroAboutComponent;
+  let componentRef: ComponentRef<HeroAboutComponent>;
   let fixture: ComponentFixture<HeroAboutComponent>;
 
   beforeEach(async () => {
@@ -14,10 +18,25 @@ describe('HeroAboutComponent', () => {
 
     fixture = TestBed.createComponent(HeroAboutComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('aboutData should be set', () => {
+    componentRef.setInput('aboutData', mocked_about_me);
+    fixture.detectChanges();
+
+    const badgeElement = fixture.debugElement.query(By.css('badge')).nativeElement;
+    const titleElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+    const imgElement = fixture.debugElement.query(By.css('img')).nativeElement;
+    const figureElement = fixture.debugElement.query(By.css('figure')).nativeElement;
+    
+    expect(badgeElement.innerText).toEqual(mocked_about_me.title);
+    expect(titleElement.innerText).toEqual(mocked_about_me.description.title);
+    expect(imgElement.src).toContain(mocked_about_me.picture);
   });
 });
