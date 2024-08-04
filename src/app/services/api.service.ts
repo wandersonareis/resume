623e150f7ce19a3102acfc8c96203d3a94ef473e
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-08-04 19:40:09 -0300

    refactor ApiService unit tests to align with updated ApiService implementation refactor(api.service.ts): remove unused HttpClient import and getFromHttp method as per code review feedback

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,20 +1,14 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
-import { Observable, of, tap } from 'rxjs';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   constructor(
-    private http: HttpClient,
     private translateService: TranslocoService
   ) { }
-
-  getFromHttp<T>(url: string): Observable<T> {
-    return this.http.get<T>(url);
-  }
 
   getLanguageData<T>(key: string): Observable<T> {
     return this.translateService.selectTranslateObject<T>(key);
