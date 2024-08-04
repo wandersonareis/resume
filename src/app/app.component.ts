91cb6cabb74f3ed08c7f6da73617b08fb96a93ba
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-08-04 19:42:10 -0300

    clean up app component template and styles for better organization add PagesModule import to root component for better module organization add ContactPageComponent to app routes for displaying contact page to users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ComponentsModule } from './components/components.module';
 import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    PagesModule,
     ComponentsModule,
     SharedModule,
 ],
-  templateUrl: './app.component.html',
+  template: `
+  <app-header-bar />
+<main>
+  <router-outlet />
+</main>
+<app-contact-page />
+<app-footer-bar />
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
