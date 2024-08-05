78351a0b32212a17232ed774cafae75d50c72edf
WandersonAReis <wandersonfrost8@hotmail.com>
2024-08-05 19:01:00 -0300

    refactor header component

--- src/app/components/navegations/header-bar/header-logo/header-logo.component.ts
+++ src/app/components/navegations/header-bar/header-logo/header-logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../../../shared/shared.module';
 
@@ -9,11 +9,22 @@ import { SharedModule } from '../../../../shared/shared.module';
     RouterModule,
     SharedModule
   ],
-  templateUrl: './header-logo.component.html',
-  styleUrl: './header-logo.component.css'
+  template: `
+  <div class="flex items-center space-x-4">
+  <a [routerLink]="routerLink()" class="flex items-center space-x-3">
+    @if (label()) {
+    <span
+      class="self-center text-2xl font-semibold whitespace-nowrap text-theme-600 dark:text-theme-50"
+      >{{ label() }}</span
+    >
+    }
+  </a>
+  <language-selector />
+</div>
+`
 })
 export class HeaderLogoComponent {
-  @Input() logo!: string;
-  @Input() label: string = '';
-  @Input() routerLink: string = '/';
+  logo = input<string | undefined>();
+  label = input<string | undefined>();
+  routerLink = input<string | undefined>();
 }
